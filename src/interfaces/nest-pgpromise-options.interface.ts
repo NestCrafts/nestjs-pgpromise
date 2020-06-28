ceb88f845eda2ce30fbd15173af55f5b3bc7917d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2020-06-28 20:19:26 +0100

    Update nest-pgpromise-options.interface.ts
    No need redefining the known types.

--- src/interfaces/nest-pgpromise-options.interface.ts
+++ src/interfaces/nest-pgpromise-options.interface.ts
@@ -1,37 +1,7 @@
+import {IConnectionParameters} from 'pg-promise/typescript/pg-subset';
+import {IInitOptions} from 'pg-promise';
+
 export interface NestPgpromiseOptions {
-  connection?:
-    | {
-        host: string;
-        port: number;
-        database: string;
-        user: string;
-        password: string;
-        ssl?: boolean;
-        binary?: boolean;
-        client_encoding?: string;
-        application_name?: string;
-        fallback_application_name?: string;
-        idleTimeoutMillis?: number;
-        max?: number;
-        query_timeout?: number;
-        keepAlive?: boolean;
-      }
-    | string;
-  initOptions?: {
-    pgFormatting?: boolean;
-    pgNative?: boolean;
-    promiseLib?: object;
-    noLocking?: boolean;
-    capSQL?: boolean;
-    noWarnings?: boolean;
-    schema?: string;
-    connect?(): any;
-    disconnect?(): any;
-    query?(): any;
-    receive?(): any;
-    task?(): any;
-    transact?(): any;
-    error?(): any;
-    extend?(): any;
-  };
+  connection?: IConnectionParameters | string;
+  initOptions?: IInitOptions;
 }
